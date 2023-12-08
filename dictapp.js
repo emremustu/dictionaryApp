9e5f5f8eeae198b5632f97d20959dd7c2bee530a
Joseph Newton <125123638+emremustu@users.noreply.github.com>
2023-12-09 02:33:17 +0300

    Update dictapp.js

--- dictapp.js
+++ dictapp.js
@@ -6,7 +6,7 @@ const soundinner = document.getElementById("ausource");
 const btn = document.getElementById("finderbutton");
 
 
-
+// An online dictionary API project that i found at youtube. I found some problems at the original version. 1- It was not running when press enter 2- The sound was not changing when word changes You can find the video here : https://www.youtube.com/watch?v=PUkgK7TI0x0&list=PLNCevxogE3fiLT6bEObGeVfHVLnttptKv&index=2&t=1322s And here is the API: https://dictionaryapi.dev/
 
 
 function Dict(){
@@ -48,7 +48,7 @@ function Dict(){
     else{
         soundatt= data[0].phonetics[0].audio;
     }    
-
+    // there is two phonetics in data. one has an audio other does not. or either does not have. So this is being checked.
 
     sound.setAttribute("src",`${soundatt}`); 
     console.log(soundatt);
